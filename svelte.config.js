b0a5391dc00af51964927454c177422cea5815aa
Oladipupo ogundipe <oladipupo.ogundipe@gmail.com>
2021-10-17 20:08:19 -0400

    4

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,12 @@ const config = {
     })],
 
 	kit: {
-		adapter: node(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 		
